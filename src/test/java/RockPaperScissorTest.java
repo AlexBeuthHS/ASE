ccbd2f34af21be3380f1321c8315921d457b5c17
Alexander Jaenisch <alexanderjaenisch@web.de>
2018-02-11 14:41:03 +0100

    fixed a test

--- src/test/java/RockPaperScissorTest.java
+++ src/test/java/RockPaperScissorTest.java
@@ -3,20 +3,21 @@ import org.junit.Test;
 
 public class RockPaperScissorTest {
 
+    private final String[] handSigns = { "r", "p","s"};
 
         @Test
         public void invalidInput() {
             RockPaperScissor game = new RockPaperScissor();
-            Assert.assertFalse(game.checkInput.typeTwo("x"));
-            Assert.assertFalse(game.checkInput.typeTwo("y"));
-            Assert.assertFalse(game.checkInput.typeTwo("1"));
+            Assert.assertFalse(game.checkInput.typeTwo("x",handSigns));
+            Assert.assertFalse(game.checkInput.typeTwo("y",handSigns));
+            Assert.assertFalse(game.checkInput.typeTwo("1",handSigns));
         }
         @Test
         public void validInput() {
             RockPaperScissor game = new RockPaperScissor();
-            Assert.assertTrue(game.checkInput.typeTwo("r"));
-            Assert.assertTrue(game.checkInput.typeTwo("p"));
-            Assert.assertTrue(game.checkInput.typeTwo("s"));
+            Assert.assertTrue(game.checkInput.typeTwo("r",handSigns));
+            Assert.assertTrue(game.checkInput.typeTwo("p",handSigns));
+            Assert.assertTrue(game.checkInput.typeTwo("s",handSigns));
         }
 
         @Test
