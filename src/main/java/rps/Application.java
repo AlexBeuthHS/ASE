c0039c5c056fee8c64b769e3411b4e0dbd283704
Alexander Jaenisch <alexanderjaenisch@web.de>
2018-02-11 19:39:58 +0100

    fixed sonar stuff

--- src/main/java/rps/Application.java
+++ src/main/java/rps/Application.java
@@ -2,7 +2,7 @@ package rps;
 
 public class Application {
 
-    public static void main(String args[]) {
+    public static void main(String[] args) {
         RockPaperScissor game = new RockPaperScissor();
         game.startGame();
     }
