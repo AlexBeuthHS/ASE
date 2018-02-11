9280258ab7b56a3ed1005ee93dc3b73f4bfa2530
Alexander Jaenisch <alexanderjaenisch@web.de>
2018-02-11 19:49:38 +0100

    fixed sonar stuff

--- src/main/java/rps/RockPaperScissor.java
+++ src/main/java/rps/RockPaperScissor.java
@@ -14,7 +14,7 @@ public class RockPaperScissor {
     public static final String YOU_WIN = "You win!";
     public static final String COMPUTER_WINS = "Computer Wins!";
 
-    private final static Map<String, String> winnerTable = Map.of(
+    private static final Map<String, String> winnerTable = Map.of(
             "rr", TIE,
             "ss", TIE,
             "pp", TIE,
@@ -25,24 +25,23 @@ public class RockPaperScissor {
             "sp", YOU_WIN,
             "ps", COMPUTER_WINS);
 
-    private final static String[] handSigns = { "r", "p","s"};
-    private String playersHand;
-
+    private static final String[] handSigns = { "r", "p","s"};
     TypeOne playedHand = () -> new Scanner(System.in).nextLine();
     TypeTwo checkInput = (String s, String[] handSigns) -> Stream.of(handSigns).anyMatch(s::equalsIgnoreCase);
     TypeThree prepareWinner = (String player, String computer) -> player+computer;
 
     public void startGame(){
+        String playersHand;
         logger.info("Welcome to my simple (R)ock, (P)aper, (S)cissor game.");
         logger.info("Chose your Hand please!");
         logger.info("Type (r) for rock, (p) for paper or (s) for scissor and press Enter");
         playersHand = playedHand.typeOne();
-        while (!checkInput.typeTwo(playersHand,handSigns)){
+        while (!checkInput.typeTwo(playersHand,this.handSigns)){
             logger.info("Oops, it seems there went something wrong, please type again!");
             playersHand = playedHand.typeOne();
         }
 
-        String computersHand = handSigns[(int)(Math.random()*3)];
+        String computersHand = this.handSigns[(int)(Math.random()*3)];
         logger.info("The computer played: " + computersHand);
         logger.info(winnerTable.get(prepareWinner.typeThree(playersHand, computersHand)));
     }
